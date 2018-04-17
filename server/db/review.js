85053db8f4604c306643c650e12601193a4cda2c
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-17 11:55:48 -0700

    finished review model

---
+++ server/db/review.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+const Review = db.define('review', {
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  }
+});
+
+module.exports = Review;
