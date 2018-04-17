ea24bb7c70a719dc8856c537ba08f82c1dad3fe5
Kẏra <kxra@riseup.net>
2018-04-17 15:22:34 -0400

    added associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,22 @@
-const db = require('./database')
-const User = require('./user')
+const db = require('./database');
+const User = require('./user');
+const Product = require('./product');
+const Category = require('./category');
+const Review = require('./review');
+const Order = require('./order');
 
-// associations go here!
+Product.belongsToMany(Category, { through: 'category-product' });
+Category.belongsToMany(Product, { through: 'category-product' });
+
+Order.belongsToMany(User, { through: 'order-user' });
+User.belongsToMany(Order, { through: 'order-user' });
+
+Review.belongsTo(Product);
+Review.belongsTo(User);
+Product.hasMany(Review);
+User.hasMany(Review);
 
 module.exports = {
   db,
   User
-}
+};
