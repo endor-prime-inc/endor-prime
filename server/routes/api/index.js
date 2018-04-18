a9764a628f67d2c02e4381da12e68fd22cdbf66c
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-17 19:42:46 -0700

    updated route index to include reviews categories and products

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,6 +3,8 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/reviews', require('./reviews'));
+router.use('/categories', require('./categories'));
+router.use('/products', require('./products'));
 
 router.use((req, res, next) => {
   const err = new Error('API route not found');
