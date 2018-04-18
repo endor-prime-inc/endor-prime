696d73f093146e580eaa80f3b8926a013c32c6fb
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-18 13:58:41 -0700

    finished initial products redux v2

--- client/store/index.js
+++ client/store/index.js
@@ -1,19 +1,17 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux'
-import logger from 'redux-logger'
-import thunks from 'redux-thunk'
-import axios from 'axios'
-import history from '../history'
-import user from './user'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import thunks from 'redux-thunk';
+import axios from 'axios';
+import history from '../history';
+import user from './user';
+import product from './product';
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({ user, product });
 
 const store = createStore(
   reducer,
-  applyMiddleware(
-    thunks.withExtraArgument({axios, history}),
-    logger
-  )
-)
+  applyMiddleware(thunks.withExtraArgument({ axios, history }), logger)
+);
 
-export default store
-export * from './user'
+export default store;
+export * from './user';
