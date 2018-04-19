2fa0365cf7959983cb8ca4ebab6cf49fc0ec9fc0
Kẏra <kxra@riseup.net>
2018-04-19 16:09:47 -0400

    ugly, but fixed bootstrap classes

--- client/components/ProductView.js
+++ client/components/ProductView.js
@@ -39,7 +39,7 @@ class ProductView extends Component {
     return (
       <div id="product-view" className="container-fluid">
         <div id="product-overview" className="row">
-          <div id="product-images" className="col-4">
+          <div id="product-images" className="col-sm-4 col-12">
             <div className="card">
               <img
                 id="selected-image"
@@ -53,44 +53,45 @@ class ProductView extends Component {
               ))}
             </div>
           </div>
-          <div id="product-info" className="col-8">
-            <div id="product-title" className="row">
-              <h1>{product.name}</h1>
-            </div>
-            <div id="product-details" className="row">
-              <div id="average-rating" className="col-3">
+          <div id="product-info" className="col-sm-8 col-12">
+            <div className="row">
+              <div id="product-title" className="col-12">
+                <h1>{product.name}</h1>
+              </div>
+              <div id="average-rating" className="col-sm-3 col-6">
                 {product.rating}
               </div>
-              <div id="product-price" className="col-3">
+              <div id="product-price" className="col-sm-3 col-6">
                 {product.price} credits
               </div>
-              <div id="product-purchase" className="col-6">
+              <div id="product-purchase" className="col-sm-6 col-12">
                 <form id="order-form" className="form-inline">
-                  <div className="form-group mb-6">
+                  <div className="form-group mb-2">
                     <label htmlFor="quantity">Quantity:</label>
                     <input
                       type="text"
                       name="quantity"
+                      className="form-control"
                       id="product-order-quantity"
                     />
                   </div>
                   <button
                     type="submit"
-                    className="btn btn-primary mb-6"
+                    className="btn btn-primary mb-2"
                     onClick={() => ''}
                   >
                     Add to Cart
                   </button>
                 </form>
               </div>
-            </div>
-            <div id="product-description" className="row">
-              <p>{product.description}</p>
+              <div id="product-description" className="col-12">
+                <p>{product.description}</p>
+              </div>
             </div>
           </div>
         </div>
         <div id="product-reviews" className="row">
-          <div id="review-header" className="row">
+          <div id="review-header" className="col-12">
             <AuthLink to={`/products/${product.id}/reviews/add`}>
               <button>Add review</button>
             </AuthLink>
@@ -98,7 +99,7 @@ class ProductView extends Component {
               <button>See all reviews</button>
             </Link>
           </div>
-          <div id="review-list" className="row">
+          <div id="review-list" className="col-12">
             <ul>
               {reviews.map(review => (
                 <li key={review.id}>
