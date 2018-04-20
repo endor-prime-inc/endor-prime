664ef4d57f3811895c0c4d5e05e8b10b2d38079c
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-19 19:29:42 -0700

    creating search components

---
+++ client/components/StoreNav.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import querystring from 'querystring';
+import { connect } from 'react-redux';
+import { getCategories } from '../store';
+import CategoryDropdown from './CategoryDropdown';
+
+class StoreNav extends Component {
+  componentDidMount = () => {
+    this.props.getCategories();
+  };
+
+  render() {
+    const { categories } = this.props;
+
+    return (
+      <div className="row">
+        <div className="col-3">
+          <CategoryDropdown categories={categories} />
+        </div>
+        <div className="col-6">
+          <CategoryDropdown categories={categories} />
+        </div>
+        <div className="col-3">
+          <CategoryDropdown categories={categories} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  const { categories } = state;
+  return { categories };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    getCategories: () => {
+      dispatch(getCategories());
+    }
+  };
+};
+
+export default connect(mapState, mapDispatch)(Products);
