96dd2d2f8dd1421c2c1f2096b6d980bf43b8b5f9
Ragheed Al-midani <ralmidani@gmail.com>
2018-04-22 00:46:18 -0400

    implement GET and PUT routes for the shopping cart

---
+++ server/routes/api/cart.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+router.get('/', (request, response, next) => {
+  try {
+    response.json(request.session.cart || {});
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/', (request, response, next) => {
+  try {
+    request.session.cart = request.body;
+    response.status(200).json(response.session.cart);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
