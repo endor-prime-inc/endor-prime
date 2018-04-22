bfa52cd05d561cdf42630a8c29f75fbdcc8061c5
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-21 21:41:01 -0700

    added some navigational components for the cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Home: there's no place like it (as long as you've logged in).
 const Cart = () => <h1>Welcome to the non-functioning cart!</h1>;
 
 export default Cart;
