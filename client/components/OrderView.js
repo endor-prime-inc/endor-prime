c76981602e46a73981021eb1d7f76ee6006852a1
Kẏra <kxra@riseup.net>
2018-04-23 22:20:20 -0400

    added prices to order view

--- client/components/OrderView.js
+++ client/components/OrderView.js
@@ -16,15 +16,31 @@ class OrderView extends Component {
         <h2>Order ID #{order.id}</h2>
         <p>Order placed: {order.createdAt}</p>
         <h3>Status: {order.status}</h3>
-        <p>
-          Recipient:<br />
+        <p>Recipient:</p>
+        <address className="mx-4">
           {order.name}
           <br />
           {order.planet}
           <br />
           {order.latitude}, {order.longitude}
-        </p>
+        </address>
+        <div className="row">
+          <div className="col-2">Quantity</div>
+          <div className="col-8">Item</div>
+          <div className="col" />
+          <div className="col">Price</div>
+        </div>
         <ProductList filteredProducts={order.products} listView={true} />
+        <div className="row">
+          <div className="col-10" />
+          <div className="col-1">Subtotal: </div>
+          <div className="col-1">
+            {order.products.reduce(
+              (acc, product) => product.quantity * product.price + acc,
+              0
+            )}
+          </div>
+        </div>
       </div>
     ) : (
       <div />
