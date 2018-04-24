78709ed2d01c27ce0f90ae4fab5d70928bb1d15e
Ragheed Al-midani <ralmidani@gmail.com>
2018-04-23 23:26:40 -0400

    remove console.log from reviews.js

--- server/routes/api/reviews.js
+++ server/routes/api/reviews.js
@@ -13,7 +13,6 @@ router.get('/', async (request, response, next) => {
 
 router.post('/', async (request, response, next) => {
   try {
-    console.log(request.user.id);
     const review = await Review.create({ ...request.body, userId: request.user.id });
     response.status(201).json(review);
   } catch (error) {
