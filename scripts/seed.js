f700987c74cceff1bb15946e50c699d5af56dcd1
Tyler Brooks <tkbrooks.github@gmail.com>
2018-04-24 18:56:22 -0700

    added pictures and changed seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -26,7 +26,10 @@ const seed = async () => {
   const categories = await Promise.all([
     Category.create({ name: 'Podracers' }),
     Category.create({ name: 'Weapons' }),
-    Category.create({ name: 'Elegant Weapons' })
+    Category.create({ name: 'Elegant Weapons' }),
+    Category.create({ name: 'Attire' }),
+    Category.create({ name: 'Ships' }),
+    Category.create({ name: 'Droids' })
   ]);
 
   const products = await Promise.all([
@@ -34,26 +37,94 @@ const seed = async () => {
       name: 'Jedi Lightsaber',
       description: 'For the good guys',
       price: 20000,
-      quantity: 2
+      quantity: 5,
+      pictures: ['/images/Jedi Lightsaber.jpg']
     }).then(product => product.setCategories(3)),
     Product.create({
       name: 'Sith Lightsaber',
       description: `You don't know the power of the dark side...`,
       price: 24000,
-      quantity: 1
+      quantity: 7,
+      pictures: ['/images/Sith Lightsaber.jpg']
     }).then(product => product.setCategories(3)),
     Product.create({
-      name: 'Light Podracer Tattooine Version',
+      name: 'Podracer #1',
       description: `Anakin's old podracer.  Mostly intact.`,
       price: 12000,
-      quantity: 3
+      quantity: 1,
+      pictures: ['/images/Podracer.png']
     }).then(product => product.setCategories(1)),
     Product.create({
       name: 'Stormtrooper Blaster',
       description: 'You rebel scum!',
       price: 2000,
-      quantity: 4
-    }).then(product => product.setCategories(2))
+      quantity: 15,
+      pictures: ['/images/Stormtrooper E-11 Blaster.png']
+    }).then(product => product.setCategories(2)),
+    Product.create({
+      name: 'Stormtrooper Suit',
+      description: 'Light and breezy',
+      price: 5000,
+      quantity: 18,
+      pictures: ['/images/Stormtrooper Suit.jpg']
+    }).then(product => product.setCategories(4)),
+    Product.create({
+      name: 'Insulated Stormtrooper Suit',
+      description: 'For those long hauls on Hoth',
+      price: 6000,
+      quantity: 10,
+      pictures: ['/images/Insulated Stormtrooper Suit.jpg']
+    }).then(product => product.setCategories(4)),
+    Product.create({
+      name: 'Rebel X-Wing',
+      description: 'Classic Rebel fighter ship',
+      price: 60000,
+      quantity: 4,
+      pictures: ['/images/Rebel X-Wing.jpg']
+    }).then(product => product.setCategories(5)),
+    Product.create({
+      name: 'Rebel Fighter Helmet',
+      description: 'Pre-wired for radio communication',
+      price: 1000,
+      quantity: 23,
+      pictures: ['/images/Rebel Fighter Helmet.jpg']
+    }).then(product => product.setCategories(4)),
+    Product.create({
+      name: 'Podracer #2',
+      description: 'Excellent handling',
+      price: 16000,
+      quantity: 2,
+      pictures: ['/images/Podracer2.jpg']
+    }).then(product => product.setCategories(1)),
+    Product.create({
+      name: 'Imperial I-class Star Destroyer',
+      description: `Don't ask us how we got this`,
+      price: 1000000,
+      quantity: 1,
+      pictures: ['/images/Imperial I-class Star Destroyer.png']
+    }).then(product => product.setCategories(5)),
+    Product.create({
+      name: 'Millennium Falcon',
+      description: `It's the ship that made the Kessel Run in less than 12 parsecs`,
+      price: 50000,
+      quantity: 1,
+      pictures: ['/images/Millennium Falcon.jpg']
+    }).then(product => product.setCategories(5)),
+    Product.create({
+      name: 'Yoda',
+      description: 'Infinite wisdom at your fingertips',
+      price: 500000,
+      quantity: 1,
+      pictures: ['/images/Yoda.jpg']
+    }).then(product => product.setCategories(3)),
+    Product.create({
+      name: 'R2D2',
+      description:
+        'Old model and highly opinionated.  May contain secret messages.  Beep boop beep.',
+      price: 10000,
+      quantity: 1,
+      pictures: ['/images/R2D2.jpg']
+    }).then(product => product.setCategories(6))
   ]);
 
   const reviews = await Promise.all([
